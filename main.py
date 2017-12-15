0568a3da2155398368565c1fb38d66fcf9e49b6e
kotrodenx <mengiowork@gmail.com>
2017-12-15 01:14:15 +0100

    new game message, basic save/load, setname/getname, exit commands

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
+import os
+from src.LlamaClass import Llama
+
+def new_game_message():
+	return 'New Game';
+
+# inizializzazione Llama
+lama = Llama('Calogero')
+
+saveFile = 'save.json';
+if (os.path.isfile(saveFile)):
+	lama.load(saveFile)
+else:
+	print new_game_message()
+	print 'baaah'
+
+
 while True :
-	cmd = raw_input("$> ")
+	cmd = raw_input("LlamaGotchi $> ")
 	print cmd
+	if (cmd == 'setname'):
+		name = raw_input("LlamaGotchi - Scegli il nome $> ")
+		lama.setName(name)
+		print 'Il nuovo nome: ' + lama.getName()
+	if (cmd == 'newllama'):lama = Llama('')
+	if (cmd == 'getname'):print lama.getName()
+	if (cmd == 'save'):lama.save(saveFile)
+	if (cmd == 'exit'):exit(0)
